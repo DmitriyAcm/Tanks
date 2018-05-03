b3f0f630f7553c942d48726738b621b141d678c4
Dmit_riy <dmitribulankin@yandex.ru>
2018-05-03 19:37:40 +0300

    Add Common Class - DynamicObject

---
+++ src/tanksgames/DynamicObject.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tanksgames;
+import Coordination.Direction;
+
+/**
+ *
+ * @author dmitr
+ */
+abstract public class DynamicObject extends Object{
+    private Direction _direct;
+    
+    DynamicObject(Direction dir)
+    {
+        super();
+        _direct = dir;
+    }
+    
+    DynamicObject(Direction dir, int PointHealth)
+    {
+        super(PointHealth);
+        _direct = dir;
+    }
+    
+    boolean moveTo(Direction dir)
+    {
+        return true;
+    }
+}
