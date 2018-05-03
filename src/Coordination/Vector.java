fc635efce23ba79c2d5855aee2bfed2e2457fad9
Dmit_riy <dmitribulankin@yandex.ru>
2018-05-03 19:24:45 +0300

    Add Coordination Class - Vector

---
+++ src/Coordination/Vector.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Coordination;
+
+/**
+ *
+ * @author dmitr
+ */
+public class Vector {
+    private Direction _dir;
+    private int _lenght;
+    
+    Vector(Direction dir, int len)
+    {
+        _dir = dir;
+        _lenght = len;
+    }
+    
+    Vector(Direction dir)
+    {
+        _dir = dir;
+        _lenght = (int)1e9;
+    }
+    
+    Direction next()
+    {
+        if(_lenght>0)
+        {
+            _lenght--;
+            return _dir;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
