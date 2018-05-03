dcf5845c978074b3381877c7ddbc79c0b8e355d9
Dmit_riy <dmitribulankin@yandex.ru>
2018-05-04 00:40:44 +0300

    Add common class Bullet

---
+++ src/tanksgames/Bullet.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tanksgames;
+import java.util.ArrayList;
+import Coordination.*;
+
+/**
+ *
+ * @author dmitr
+ */
+public abstract class Bullet extends DynamicObject {
+    private int _LenghtFlight;
+    
+    Bullet(Direction dir, int lenFly, GameField field)
+    {
+        super(dir,field);
+        _LenghtFlight = lenFly;
+    }
+    
+    public abstract ArrayList<Vector> traectory();
+    
+    @Override
+    public boolean AirBlocks()
+    {
+        return true;
+    }
+    
+    @Override
+    public boolean GroundBlocks()
+    {
+        return false;
+    }
+    
+    @Override
+    public void DamageObject()
+    {
+    
+    }
+}
